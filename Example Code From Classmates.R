a5af2b0059c81419f1401a4997d60c1bef3b5205
james.j.mccammon@gmail.com <jamesmccammon@Jamess-iMac.local>
2014-11-11 23:34:19 -0800

    useful code from classmates

---
+++ Example Code From Classmates.R
@@ -0,0 +1,95 @@
+# Example code
+# zelite/ExData_Plotting1
+library(dplyr)
+
+
+Sys.setlocale(category = "LC_ALL", "C") #to have the weekday lables in english
+# READING FILE
+# there's a row every minute, so the rows to import were calculated to only have the rows needed for the assignment
+
+startFile <- as.POSIXct("2006-12-16 17:24:00")
+startTime <- as.POSIXct(" 2007-02-01 00:00:00")
+endTime <- as.POSIXct("2007-02-03 00:00:00")
+
+startRow <- as.numeric(difftime(startTime, startFile, units="mins"))+1 # "+1" because headers also count as row
+nRows <- as.numeric(difftime(endTime, startTime, units="mins"))
+
+#create a connection to the zip file
+con <- unz(description = "exdata_data_household_power_consumption.zip", 
+           filename = "household_power_consumption.txt")
+
+#read the headers
+headers <- unlist(str_split(readLines(con, n = 1), ";"))
+#read the data from connection
+data.to.plot <- read.table(con, header = FALSE, sep=";",
+                           colClasses = c("character", "character", rep("numeric", 7)), 
+                           na.strings = "?", skip=startRow, nrow=nRows)
+#in read.table colClasses are defined to speed things up. 
+
+#put the headers on the data
+names(data.to.plot) <- headers
+
+
+data.to.plot <- as.tbl(data.to.plot)
+#dplyr does not support POSIXlt, so need to explicity define POSIXct
+data.to.plot <- mutate(data.to.plot, 
+                       datetime = as.POSIXct(strptime(paste(Date, Time), format = "%d/%m/%Y %T")))
+
+#Now the plot code
+png(filename = "plot4.png", width = 480, height = 480)
+
+par(mfcol =  c(2,2) )
+
+#first plot
+plot(x=data.to.plot$datetime, y = data.to.plot$Global_active_power,
+     type="l", xlab="", ylab="Global Active Power")
+#second plot
+plot(x=data.to.plot$datetime, y = data.to.plot$Sub_metering_1,
+     type="l", xlab="", ylab="Energy sub metering")
+lines(x=data.to.plot$datetime, y = data.to.plot$Sub_metering_2,
+      col = "red")
+lines(x=data.to.plot$datetime, y = data.to.plot$Sub_metering_3,
+      col = "blue")
+legend(x="topright", legend = paste0("Sub_metering_", 1:3), lty = rep(1,3),
+       col = c("black", "red", "blue"), bty="n")
+#third plot
+plot(x = data.to.plot$datetime, y = data.to.plot$Voltage, type = "l", 
+     xlab="datetime", ylab="Voltage")
+#fourth plot
+plot(x = data.to.plot$datetime, y = data.to.plot$Global_reactive_power, type = "l",
+     xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
+
+# Example 2
+# JerryChongyuanXu/ExData_Plotting1
+
+data_all <- read.csv("household_power_consumption.txt", header = TRUE, sep = ';', stringsAsFactor = FALSE, dec = ".")
+data_all$Date <- as.Date(data_all$Date, format = "%d/%m/%Y")
+
+data <- subset(data_all, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_all)
+
+datetime <- paste(as.Date(data$Date), data$Time)
+data$datetime <- as.POSIXct(datetime)
+
+data$Global_active_power <- as.numeric(data$Global_active_power)
+
+Sub_metering_1 <- as.numeric(data$Sub_metering_1)
+Sub_metering_2 <- as.numeric(data$Sub_metering_2)
+Sub_metering_3 <- as.numeric(data$Sub_metering_3)
+
+par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
+
+with(data, {
+  plot(datetime, Global_active_power, type = "l", ylab = "Global Active Power", xlab = "")
+  plot(datetime, Voltage, type = "l", ylab = "Voltage", xlab = "")
+  plot(datetime, Sub_metering_1, type = "l", ylab = "Voltage", xlab = "")
+  lines(datetime, Sub_metering_2, col = 'Red')
+  lines(datetime, Sub_metering_3, col = 'Blue')
+  legend("topright", col=c("black", "red", "blue"), lty = 1, lwd =2 , legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(datetime, Global_reactive_power, type = "l", ylab="Global Rective Power",xlab="")
+})
+
+dev.copy(png, file = "plot4.png", height = 480, width = 480)
+dev.off()
